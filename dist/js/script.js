e77b39287e4c1ac2fb17058d61a03ec57f4820a5
rssllbnvj <russell_buenviaje@dlsl.edu.ph>
2023-08-04 21:34:52 +0800

    coffee commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,35 @@
 const headerNav = document.querySelector(".header__nav");
 const toggleMenu = document.querySelector(".toggle__menu");
+const buhokoverlay = document.querySelector(".buhok");
 toggleMenu.addEventListener("click", () => {
     headerNav.classList.toggle("open");
     toggleMenu.classList.toggle("open");
+    buhokoverlay.classList.toggle("open");
 });
+
+
+const kalboNav = document.querySelectorAll(".kalbo__nav__link");
+const kalboContent = document.querySelectorAll(".kalbo__item__content");
+kalboNav.forEach((kalbo) => {
+    kalbo.addEventListener("click", () => {
+        removeActiveKalbo();
+        kalbo.style = "transition:.5s ease";
+        kalbo.classList.add("active");
+        kalbo.style = "transition:.5s ease";
+        const activeContent = document.querySelector(`#${kalbo.id}-content`);
+        removeActiveContent();
+        activeContent.classList.add("active");
+    });
+});
+
+function removeActiveKalbo() {
+    kalboNav.forEach((kalbo) => {
+        kalbo.classList.remove("active");
+    });
+}
+
+function removeActiveContent() {
+    kalboContent.forEach((kalbo) => {
+        kalbo.classList.remove("active");
+    });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: ["./dist/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        light: '#ffffff',
-        dark: '#000000',
-        accent: '#00754a'
-      }
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                light: '#ffffff',
+                dark: '#000000',
+                accent: '#00754a',
+                daccent: '#1e3932',
+                ligreen: '#f1f8f5',
+                megreen: '#d4e9e2'
+            }
+        },
     },
-  },
-  plugins: ['prettier-plugin-tailwindcss'],
+    plugins: ['prettier-plugin-tailwindcss'],
 }
